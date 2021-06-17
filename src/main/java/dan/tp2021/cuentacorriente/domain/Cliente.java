1c15da036de36df235bc786227f3a408dcae8efc
Franco Perino <fperino@frsf.utn.edu.ar>
2021-06-16 22:26:57 -0300

    se agrego la restriccion de solo lectura que se me habia pasado por alto

--- src/main/java/dan/tp2021/cuentacorriente/domain/Cliente.java
+++ src/main/java/dan/tp2021/cuentacorriente/domain/Cliente.java
@@ -8,7 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.transaction.annotation.Transactional;
+
 @Entity
+@Transactional(readOnly = true)
 public class Cliente {
 	
 	@Id
