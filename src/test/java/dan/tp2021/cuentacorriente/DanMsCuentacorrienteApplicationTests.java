3a7fceb76cde7a5779f565126a18c972577fbc13
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-07-14 22:35:29 -0300

    Agregado de perfil de testing y produccion. Falta completar

--- src/test/java/dan/tp2021/cuentacorriente/DanMsCuentacorrienteApplicationTests.java
+++ src/test/java/dan/tp2021/cuentacorriente/DanMsCuentacorrienteApplicationTests.java
@@ -2,8 +2,10 @@ package dan.tp2021.cuentacorriente;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("testing")
 class DanMsCuentacorrienteApplicationTests {
 
 	@Test
--- src/test/java/dan/tp2021/cuentacorriente/PagoRestTest.java
+++ src/test/java/dan/tp2021/cuentacorriente/PagoRestTest.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
 import dan.tp2021.cuentacorriente.domain.Cliente;
@@ -25,7 +26,7 @@ import dan.tp2021.cuentacorriente.service.dao.ClienteRepository;
 import dan.tp2021.cuentacorriente.service.interfaces.PagoService;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-
+@ActiveProfiles("testing")
 public class PagoRestTest {
 
 	private String ENDPOINT_PAGO = "/api/pago";
